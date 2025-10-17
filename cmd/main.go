23f3ef226bd8f2948c124a0becc7ce8b0a2b50e1
Sebastian <94076238+fseba@users.noreply.github.com>
2025-10-17 21:17:11 +0200

    feat(healh_check): Add health check handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/fseba/hello-api/handlers"
 	"github.com/fseba/hello-api/handlers/rest"
 )
 
 func main() {
-	addr := ":8080"
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if addr == ":" {
+		addr = ":8080"
+	}
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck)
 
 	log.Printf("listening on %s\n", addr)
 
