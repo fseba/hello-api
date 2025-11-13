77eea64f47726b5b16a5adf8b147cb0f691e3839
Sebastian Frueh <sfrueh@gmx.net>
2025-11-13 21:52:29 +0100

    Add tests

---
+++ translation/client.go
@@ -0,0 +1,52 @@
+package translation
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+)
+
+var _ HelloClient = &APIClient{}
+
+type APIClient struct {
+	endpoint string
+}
+
+// NewHelloClient creates a new HelloClient with the given endpoint.
+func NewHelloClient(endpoint string) *APIClient {
+	return &APIClient{endpoint: endpoint}
+}
+
+// Translate will call external client for translation.
+func (c *APIClient) Translate(word, language string) (string, error) {
+	req := map[string]any{
+		"word":     word,
+		"language": language,
+	}
+	b, err := json.Marshal(req)
+	if err != nil {
+		return "", errors.New("unable to encode msg")
+	}
+
+	resp, err := http.Post(c.endpoint, "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		log.Println(err)
+		return "", errors.New("call to api failed")
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		return "", nil
+	}
+	if resp.StatusCode == http.StatusInternalServerError {
+		return "", errors.New("error in api")
+	}
+	b, _ = io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	var m map[string]any
+	if err := json.Unmarshal(b, &m); err != nil {
+		return "", errors.New("unable to decode api response")
+	}
+	return m["translation"].(string), nil
+}
